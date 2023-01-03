0152f593d8cc88852fba215dff96e480c025ecbc
akinwale ayomide <akinwaleayomide55@gmail.com>
2023-01-03 20:06:51 +0100

    ui design for header partially added

--- frontend/src/Components/Miscellaneous/SideDrawer.js
+++ frontend/src/Components/Miscellaneous/SideDrawer.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import { Box, Button, Tooltip } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 const SideDrawer = () => {
-  return <div>SideDrawer</div>;
+  const [search, setSearch] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingChat, setLoadingChat] = useState(false);
+
+  return (
+    <>
+      <Box>
+        <Tooltip label='Search Users t chat' hasArrow placement='bottom-end'>
+          <Button variant='ghost'></Button>
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
 
 export default SideDrawer;
