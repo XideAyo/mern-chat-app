ae1d44a52f2b8d733a0315d7f01749a69b6202b8
akinwale ayomide <akinwaleayomide55@gmail.com>
2023-01-06 20:21:17 +0100

    side drawer partially done

--- frontend/src/Components/Miscellaneous/SideDrawer.js
+++ frontend/src/Components/Miscellaneous/SideDrawer.js
@@ -9,6 +9,15 @@ import {
   MenuList,
   Text,
   Tooltip,
+  Drawer,
+  useDisclosure,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  Input,
+  Toast,
+  useToast,
 } from '@chakra-ui/react';
 import { BellIcon, ChevronDownIcon } from '@chakra-ui/icons';
 import React, { useState } from 'react';
@@ -25,9 +34,50 @@ const SideDrawer = () => {
   const { user } = ChatState();
   const navigate = useNavigate();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const logOutHandler = () => {
     localStorage.removeItem('userInfo');
     navigate('/');
+  };
+
+  const toast = useToast();
+
+  const handleSearch = async () => {
+    if (!search) {
+      toast({
+        title: 'Please Enter something to search',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-left',
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.get(`/api/user?search=${search}`, config);
+
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: 'Error Occured',
+        description: 'Failed to load the search results',
+        status: 'warning',
+        duration: 5000,
+        isClosable: true,
+        position: 'bottom-left',
+      });
+    }
   };
 
   return (
@@ -42,7 +92,7 @@ const SideDrawer = () => {
         display='flex'
       >
         <Tooltip label='Search Users to chat' hasArrow placement='bottom-end'>
-          <Button variant='ghost'>
+          <Button variant='ghost' onClick={onOpen}>
             <Text d={{ base: 'none', md: 'flex' }} px='4'>
               Search User
             </Text>
@@ -78,6 +128,24 @@ const SideDrawer = () => {
           </Menu>
         </div>
       </Box>
+
+      <Drawer placement='left' onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth='1px'>Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display='flex' pb={2}>
+              <Input
+                placeholder='Search by name or email'
+                mr={2}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </>
   );
 };
